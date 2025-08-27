6ecd40fc539f71bf2b59607eb4b547d0008b863b
Varshith K <varshithknaik@gmail.com>
2025-08-27 12:24:52 +0530

    feat: add CSS loaders and integrate styles into the application

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import '../styles/index.css'
+
 import { onLoadEventHandler , newTodoEventHandler , removeTodoEventHandler , toggleTodoEventListener } from "./event-handlers"
 
 window.addEventListener('load', onLoadEventHandler)
